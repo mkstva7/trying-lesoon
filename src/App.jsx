d19017c47beb155ec7f49069b5fc98865c4d6dcd
mkstva7 <maksutovamilana7@gmail.com>
2023-10-22 12:02:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Backgruod } from "./conponent/Backgruod"
+
+export const App = () => {
+  return(
+    <div> 
+      <span>App</span>
+      <Backgruod/>
+    </div>
+  )
+}
---
+++ src/conponent/Backgruod.jsx
@@ -0,0 +1,8 @@
+import styles from './Backdruod.module.css'
+export const Backgruod = () => {
+    return (
+        <div className={styles.container}>
+            <h2>Backgruod</h2>
+        </div>
+    )
+}
